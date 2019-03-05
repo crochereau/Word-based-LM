34eb0d2dd186d51b604b665f91f3e54cadd0b327
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2019-03-05 23:44:24 +0100

    Add back from refactoring

---
+++ Michael_experiments/paths.py
@@ -0,0 +1,25 @@
+
+
+CHAR_VOCAB_HOME = "/vocabularies" # this is part of the repo
+
+MODELS_HOME = "/Users/charlotterochereau/Documents/Stage_ENS/Checkpoints/"  # for storing neural network parameters
+LOG_HOME = "/Users/charlotterochereau/Documents/Stage_ENS/Checkpoints/" # for logging validation losses
+FIGURES_HOME = "//Users/charlotterochereau/Documents/Stage_ENS/Checkpoints/" # for storing visualizations
+WIKIPEDIA_HOME = "/Users/charlotterochereau/Documents/Stage_ENS/German_corpora" # location of Wikipedia corpora
+
+MSR_COMP_HOME = "/Users/charlotterochereau/Documents/Stage_ENS/"
+
+
+
+##########
+# to be on the safe side
+CHAR_VOCAB_HOME += "/"
+MODELS_HOME += "/" 
+LOG_HOME += "/" 
+FIGURES_HOME += "/"
+WIKIPEDIA_HOME += "/"
+
+
+UD_HOME = "/u/scr/mhahn/FAIR18/UD/"
+
+
